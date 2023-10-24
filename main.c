2e227e1ca5dd8e01ff110faff428dc55276c9084
abdosn <abdonassar283@gmail.com>
2023-10-24 23:08:27 +0300

    Added main flow of system

--- main.c
+++ main.c
@@ -11,19 +11,75 @@
 
 
 #include <stdio.h>
+#include <string.h>
+
 
 int mode = 0;
-
+char SavedPassword[30] = "1234";
 void main(void)
 {
     scanf("1-Admin mode\n2-User mode\nEnter mode number : %d" , &mode);
 
-    if(mode == 1)
+    if(mode == 1) // Admin Mode
     {
-        
+        int trails = 3 ;
+        char EnteredPassword[30];
+        while(trails != 0)
+        {
+            scanf("Enter Password : %s",&EnteredPassword);
+            if(strcmp(SavedPassword , EnteredPassword) == 0)
+            {
+                printf("Access Granted\n");
+                break;                
+            }
+            else
+            {
+                trails --;
+                printf("Wrong password\nYou have %d trails left",trails);
+                if(trails == 0)
+                {
+                    return;
+                }
+            }
+        }
+        int AdminMode;
+        scanf("Options Avaliable\n1-Add new patient record\n2-Edit patient record\n3-Reserve a slot with the doctor\n4-Cancel reservation\nEnter option number : %d",&AdminMode);
+        switch (AdminMode)
+        {
+        case 1:
+            /* code */
+            break;
+        case 2:
+
+            break;
+        case 3:
+
+            break;
+
+        case 4:
+
+            break;
+        default:
+            break;
+        }
+
     }
     else if(mode == 2)
     {
+        int UserMode;
+        scanf("Options Avaliable\n1-View patient record\n2-View today's reservations\nEnter option number : %d",&UserMode);
+        switch (UserMode)
+        {
+        case 1:
+            /* code */
+            break;
+        
+        case 2:
+
+            break;
+        default:
+            break;
+        }
 
     }
     else
